460f3c545a42161d1af3043efef47c621350e0dc
Lasse <lasseh0310@gmail.com>
2020-05-29 13:57:15 +0200

    main framework finished

--- helpers.R
+++ helpers.R
@@ -14,3 +14,31 @@ dt_options <- function(dom_settings = "tip"){
   autoWidth = TRUE,
   columnDefs = list(list(width = '20%', targets = "_all")))
 }
+
+
+shape_opts <- function(){
+  c("None" = "NULL",
+    "Square" = 0,
+    "Circle" = 1,
+    "Triangle" = 2,
+    "Plus" = 3,
+    "Cross" = 4,
+    "Diamond" = 5,
+    "Downwards triangle" = 6,
+    "Square cross" = 7,
+    "Star" = 8)
+}
+
+palette_opts <- function(){
+  list(
+    "Default" = "NULL",
+    Discrete = c(
+      "Dark2" = "Dark2",
+      "Greens" = "Greens",
+      "YlOrRd" = "YlOrRd"),
+    Continuous = c(
+      "Viridis" = "viridis",
+      "Inferno" = "inferno",
+      "Plasma" = "plasma")
+  )
+}
