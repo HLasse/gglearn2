b6d0134f93c6f37d278ec6fe53a906ba2910386a
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2020-05-30 20:20:26 +0200

    added package

---
+++ gglearn2/R/gglearn2.R
@@ -0,0 +1,10 @@
+#'gglearn2
+#'
+#'A package for teaching ggplot2
+#'
+#'@docType package
+#'
+#'@author L. Hansen \email{lasseh0310@gmail.com} and K. Enevoldsen \email{kennethcenevoldsen@gmail.com}
+#'
+#'@name gglearn2
+NULL
