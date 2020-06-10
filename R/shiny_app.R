52adaa7bb431a6362c33f71314b27228a6f284d9
Lasse <lasseh0310@gmail.com>
2020-06-10 13:48:05 +0200

    typo

--- R/shiny_app.R
+++ R/shiny_app.R
@@ -347,7 +347,7 @@ gglearn <- function(dataset){
                                        p(a("ggplot2: Elegant Graphics for Data Analysis", href="https://ggplot2-book.org/")),
                                        br(),
                                        h4("Free Online Courses:"),
-                                       p(a("AndreW Heiss' Data Visualization Course", href="https://datavizm20.classes.andrewheiss.com/")),
+                                       p(a("Andrew Heiss' Data Visualization Course", href="https://datavizm20.classes.andrewheiss.com/")),
                                        br(),
                                        h4("Misc."),
                                        p(a("Cheatsheets from RStudio", href="https://rstudio.com/resources/cheatsheets/")),
